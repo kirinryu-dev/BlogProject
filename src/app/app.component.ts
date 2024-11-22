d4946a0da703529538b3a5ad9aa097ffe4db2222
kirinryu.dev <kirinryu.dev@gmail.com>
2024-11-22 12:28:09 +0000

    another little bit of code start little and continue the fun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component' ;
+import { HeaderComponent } from './layout/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , HomeComponent],
+  imports: [RouterOutlet , HeaderComponent ,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
